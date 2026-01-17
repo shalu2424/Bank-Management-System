24c030ebd7e0b18831f380f0d104b16e72e4c6be
shalu2424 <132142814+shalu2424@users.noreply.github.com>
2026-01-17 15:16:14 +0530

    Add files via upload

---
+++ BankAccount.java
@@ -0,0 +1,38 @@
+package com.bank.bankmanagementsytem;
+public class BankAccount {
+
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    public void withdraw(double amount) throws InsufficientBalanceException {
+        if (amount > balance) {
+            throw new InsufficientBalanceException("❌ Insufficient Balance!");
+        }
+        balance -= amount;
+    }
+
+    public void displayDetails() {
+        System.out.println("Account Number : " + accountNumber);
+        System.out.println("Account Holder : " + accountHolderName);
+        System.out.println("Balance        : ₹" + balance);
+    }
+}
